e8190cb5e35f51a99dc2e9765bf1cb4ac24cf30e
vagrant <vagrant@precise32.(none)>
2015-02-07 04:57:34 +0000

    minor error

--- Userdata.py
+++ Userdata.py
@@ -10,7 +10,7 @@ def create_simple_user(info, Name, Friends, Events):
 
 if __name__ == '__main__':
         info = dict()
-        create_simple_user(info, "Sihan", ["Zhou, Gushan"], ["Devest, World Cup"])
+        create_simple_user(info, "Sihan", ["Zhou", "Gushan"], ["Devest", "World Cup"])
        	create_simple_user(info, "Gushan", ["Zhou", "Xiwei"], [])
        	create_simple_user(info, "Xiwei", ["Zhou", "Gushan"], ["Devest"])
 	create_simple_user(info, "Zhou", ["Sihan", "Gushan"], ["Devest", "World Cup"])
